6f3579d0e83175fcec17d8a5b2cdb5ef5744f07c
Gelli4k <gelli4ka@mail.ru>
2022-04-26 23:15:26 +0300

    html-страница с описанием цветка Flask приложение, которое работает с небольшой текстовой базой данных пользователей. Данные загружаются из файла JSON, работа с данными  вынесена в функции

--- utils.py
+++ utils.py
@@ -2,16 +2,16 @@ import json
 
 
 def load_candidates(path):
-    """загрузка всех кандидатов"""
-    with open(path, "r", encoding="UTF-8") as file:
-        return json.load(file)
+    """загрузка всех кандидатов списком"""
+    with open(path, "r", encoding="UTF-8") as candidate_list:
+        return json.load(candidate_list)
 
 
 def get_candidates(candidate_id):
     """ вывод кандидатов по id"""
     candidates = load_candidates("candidates.json")
     for candidate in candidates:
-        if candidate['id'] == candidate_id:
+        if candidate["id"] == candidate_id:
             return candidate
     return None
 
@@ -22,7 +22,7 @@ def get_candidates_by_name(candidate_name):
     candidates = load_candidates("candidates.json")
     matches = []
     for candidate in candidates:
-        if candidate_name.lower() in candidate['name'].lower():
+        if candidate_name.lower() in candidate["name"].lower():
             matches.append(candidate)
     return matches
 
@@ -32,7 +32,7 @@ def get_candidates_by_skill(skill_name):
     candidates = load_candidates("candidates.json")
     matches = []
     for candidate in candidates:
-        if skill_name.lower() in candidate['skills'].lower().split(", "):
+        if skill_name.lower() in candidate["skills"].lower().split(", "):
             matches.append(candidate)
     return matches
 
